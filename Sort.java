7ad33016d46067af12f0b1ee445e67d598e0e198
sunada <sunada2005@163.com>
2013-08-27 23:03:24 +0800

    add quick sorting

--- Sort.java
+++ Sort.java
@@ -54,65 +54,63 @@ public class Sort{
 
     public int[] insertion(int[] arr){
         int dup[] = this.dup_arr(arr);
-        int len = dup.length;
-        int key = 0;
-        int tmp = 0;
-        for (int i = 0; i < len; i++){
-            key = 0;
-            for(int j = i+1; j < len; j++){
-                while (dup[j] <= dup[key] && key < j){
-                    key++;
-                }
-                if (key == j){
-                    break;
-                }
-                tmp = dup[j];
-                while(j > key){
-                    dup[j] = dup[j-1];
-                    j--;
-                }
-                dup[key] = tmp;
+        for (int i = 1; i < dup.length; i++){
+            int key = dup[i];
+            int j = i - 1;
+            while (j >= 0 && dup[j] < key){
+                dup[j + 1] = dup[j];
+                j--;
             }
+            dup[j + 1] = key;
         }
         return dup;
     }
 
     public int[] shell(int[] arr, int step){
         int dup[] = this.dup_arr(arr);
-        int len = dup.length;
-        int i = 0;
-        int j = 0;
-        int key = 0;
-        int tmp = 0;
-        int flag = 0;
         while (step > 0){
             //insertion sorting
-            for (i = 0; i < step; i++){
-                for (j = i + step; j < len; j += step){
-                    key = i;
-                    while (dup[j] <= dup[key] && key < j){
-                        key += step;
-                    }
-                    if (key == j){
-                        if (dup[key - step] >= dup[j]){
-                            continue;
-                        }
-                    }
-
-                    tmp = dup[j];
-                    flag = j;
-                    while (j > key){
-                        dup[j] = dup[j - step];
-                        j -= step;
-                    }
-                    dup[key] = tmp;
-                    j = flag;
+            for (int i = step; i < dup.length; i += step){
+                int j = i - step;
+                int key = dup[i];
+                while (j >= 0 && dup[j] < key){
+                    dup[j + step] = dup[j];
+                    j -= step;
                 }
+                dup[j + step] = key;
             }
             step = step / 2;
-            this.shell(dup, step);
         }
         return dup;
+    }
+
+    public void quick(int[] dup, int leftpoint, int rightpoint){
+        if (leftpoint >= rightpoint){
+            return;
+        }
+        int key = dup[rightpoint];
+        int left = leftpoint;
+        int right = rightpoint;
+        while (true){
+            while (left < right && dup[left] >= key){
+                left++;
+            }
+            while (left < right && dup[right] <= key){
+                right--;
+            }
+            int tmp = 0;
+            if (left >= right){
+                tmp = dup[right];
+                dup[right] = key;
+                dup[rightpoint] = tmp;
+                break;
+            }
+            tmp = dup[right];
+            dup[right] = dup[left];
+            dup[left] = tmp;
+        }
+        this.quick(dup, leftpoint, left - 1);
+        this.quick(dup, left + 1, rightpoint);
     }
 
     public static void main(String[] args){
@@ -132,5 +130,10 @@ public class Sort{
 
         System.out.print("after shell sorting:       ");
         s.print_arr(s.shell(arr, arr.length/2));
+
+        System.out.print("after quick sorting:       ");
+        int dup[] = s.dup_arr(arr);
+        s.quick(dup, 0, dup.length - 1);
+        s.print_arr(dup);
     }
 }
