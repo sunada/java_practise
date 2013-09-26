2e9fd1873b091929354ebbd108267d9244a9cddb
unknown <sunada2005@163.com>
2013-09-26 16:32:33 +0800

    bubble, selection, insertion, shell, quick, heap, counting, merge sorting

--- Sort.java
+++ Sort.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Sort{
     public void print_arr(int[] arr){
         int len = arr.length;
@@ -7,6 +9,7 @@ public class Sort{
         System.out.println("");
     }
 
+
     public int[] dup_arr(int[] arr){
         int len = arr.length;
         int re[] = new int[len];
@@ -15,6 +18,7 @@ public class Sort{
         }
         return re;
     }
+
 
     public int[] bubble(int[] arr){
         int dup[] = this.dup_arr(arr);
@@ -31,6 +35,7 @@ public class Sort{
         }
         return dup;
     }
+
 
     public int[] selection(int[] arr){
         int dup[] = this.dup_arr(arr);
@@ -52,6 +57,7 @@ public class Sort{
         return dup;
     }
 
+
     public int[] insertion(int[] arr){
         int dup[] = this.dup_arr(arr);
         for (int i = 1; i < dup.length; i++){
@@ -65,6 +71,7 @@ public class Sort{
         }
         return dup;
     }
+
 
     public int[] shell(int[] arr, int step){
         int dup[] = this.dup_arr(arr);
@@ -83,6 +90,7 @@ public class Sort{
         }
         return dup;
     }
+
 
     public void quick(int[] dup, int leftpoint, int rightpoint){
         if (leftpoint >= rightpoint){
@@ -112,6 +120,176 @@ public class Sort{
         this.quick(dup, leftpoint, left - 1);
         this.quick(dup, left + 1, rightpoint);
     }
+   
+
+    //min heap
+    public int micro_adjust(int[] arr, int i, int last){
+        if (2 * i + 1 > last){
+            return i;
+        }
+
+        int max = arr[i] < arr[2 * i + 1] ? i : (2 * i + 1);
+        if (2 * i + 2 > last){
+            return max;
+        }
+
+        max = arr[max] < arr[2 * i + 2] ? max : (2 * i + 2);
+        if (max != i){
+            int tmp = arr[i];
+            arr[i] = arr[max];
+            arr[max] = tmp;
+        }
+
+        return max;
+    }
+    //part of heap sorting. build a min heap
+    public void build_heap(int[] arr, int last){
+        int mid = (last + 1) / 2 - 1;
+        for (int i = mid; i >= 0; i--){
+            int tmp = micro_adjust(arr, i, last);
+            //System.out.print("i: " + i + " tmp: " + tmp + "arr: ");
+            //this.print_arr(arr);
+            int flag = i;
+            while (tmp <= mid && tmp != flag){
+                flag = tmp;
+                tmp = micro_adjust(arr, tmp, last);
+            }
+        }
+        return;
+    }
+    //part of heap sorting. adjust the left arr
+    public void adjust_heap(int[] arr, int last){
+        int tmp = arr[0];
+        arr[0] = arr[last];
+        arr[last] = tmp;
+        this.build_heap(arr, last - 1);
+        return;
+    }
+    //use min heap to sort arr from big to small
+    public int[] heap(int[] arr){
+        int dup[] = this.dup_arr(arr);
+        this.build_heap(dup, dup.length - 1);
+        for (int i = 0; i < dup.length; i++){
+            //System.out.print("i: " + i + " arr: ");
+            //this.print_arr(dup);
+            adjust_heap(dup, dup.length - 1 - i);
+        }
+        return dup;
+    }        
+
+
+    public int[] counting(int[] arr){
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            min = min < arr[i] ? min : arr[i];
+            max = max > arr[i] ? max : arr[i];
+        }
+
+        int buckets[] = new int[max - min + 1];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++){
+            index = arr[i] - min;
+            buckets[index]++;
+        }
+
+        int res[] = new int[arr.length];
+        for (int i = 0; i < res.length; i++){
+            res[i] = -1;
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            index = arr[i] - min;
+            int cnt = 0;
+            for (int j = 0; j < index; j++){
+                cnt += buckets[j];
+            }
+            while (res[res.length - 1 - cnt] != -1){
+                cnt++;
+            }
+            res[res.length - 1 - cnt] = arr[i];
+        }
+        return res;
+    }
+
+
+    /*Failure: don't know how to build the buckets arr ......
+    public int[] radix(int[] arr){
+        int dup[] = this.dup_arr(arr);
+
+        Queue[] buckets = new Queue[10];
+        for (int i = 0; i < 10; i++){
+            buckets[i] = new LinkedList();
+        }
+
+        int max = dup[0];
+        for (int i = 1; i < dup.length; i++){
+            max = max > dup[i] ? max : dup[i];
+        }
+        
+        int n = 10;
+        while (max > 0){
+            for (int i = 0; i < dup.length; i++){
+                buckets[dup[i] % n].add(dup[i]);
+            }
+            int j = 0;
+            for (int i = 0; i < 10; i++){ 
+                while (!buckets[i].isEmpty()){
+                    dup[j++] = buckets[i].getFirst();
+                    buckets[i].removeFirst();
+                }
+            }
+            max /= 10;
+            n *= 10;
+        }
+        return dup;
+    }
+    */
+
+
+    //part of merge sorting: merge two arr together
+    public int[] merge(int[] arr1, int[] arr2){
+        int res[] = new int[arr1.length + arr2.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < arr1.length && j < arr2.length){
+            if (arr1[i] > arr2[j]){
+                res[k++] = arr1[i++];
+            }else{
+                res[k++] = arr2[j++];
+            }
+        }
+        
+        while (i < arr1.length){
+            res[k++] = arr1[i++];
+        }
+        while (j < arr2.length){
+            res[k++] = arr2[j++];
+        }
+        return res;
+    }
+    //the main part of merge_sort
+    public int[] merge_sort(int[] arr, int start, int end){
+        //System.out.println("start: " + start + " end: " + end);
+        if (end - start < 1){
+            //System.out.println("end == start");
+            int tmp[] = {arr[start]};
+            return tmp;
+        }
+        
+        int flag = (end - start) / 2;
+        int res1[] = merge_sort(arr, start, start + flag);
+        //System.out.println("res1:");
+        //this.print_arr(res1);
+        int res2[] = merge_sort(arr, start + flag + 1, end);
+        //System.out.println("res2:");
+        //this.print_arr(res2);
+        int res[] = merge(res1, res2);
+        //this.print_arr(res);
+        return res;
+    }
+
 
     public static void main(String[] args){
         int arr[] = {6, 6, 7, 5, 3, 2, 4, 1, 8, 9, 0};
@@ -135,5 +313,23 @@ public class Sort{
         int dup[] = s.dup_arr(arr);
         s.quick(dup, 0, dup.length - 1);
         s.print_arr(dup);
+
+        System.out.print("after heap sorting:        ");
+        s.print_arr(s.heap(arr));
+
+        System.out.print("after counting sorting:    ");
+        s.print_arr(s.counting(arr));
+        
+        //failure: because don't know how to define buckets
+        //System.out.print("after radix sorting:       ");
+        //s.print_arr(s.radix(arr));
+        
+        //failure: because don't know how to define buckets
+        //System.out.print("after buckets sorting:       ");
+        //s.print_arr(s.buckets(arr));
+
+        System.out.print("after merge sorting:       ");
+        dup = s.dup_arr(arr);
+        s.print_arr(s.merge_sort(dup, 0, dup.length - 1));
     }
 }
